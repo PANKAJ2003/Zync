89b24dd5cb52f8d9f50500af4614c28002a4e349
PANKAJ2003 <pankajmishra84454@gmail.com>
2026-06-19 19:53:50 +0530

    Enhance orchestrator service with task execution - Integrated `WorkflowStepRepository` for step retrieval. - Updated `WebhookEventConsumer` to initiate task execution events and send them to Kafka. - Added logic to fetch and process workflow steps.

--- orchestrator-service/src/main/java/com/zync/orchestratorservice/consumer/WebhookEventConsumer.java
+++ orchestrator-service/src/main/java/com/zync/orchestratorservice/consumer/WebhookEventConsumer.java
@@ -1,14 +1,19 @@
 package com.zync.orchestratorservice.consumer;
 
 import com.zync.domain.enums.RunStatus;
+import com.zync.domain.events.TaskExecutionEvent;
 import com.zync.domain.events.WebhookEvent;
 import com.zync.orchestratorservice.entity.ExecutionRun;
+import com.zync.orchestratorservice.entity.WorkflowStep;
 import com.zync.orchestratorservice.repository.ExecutionRunRepository;
 import com.zync.orchestratorservice.repository.WorkflowRepository;
+import com.zync.orchestratorservice.repository.WorkflowStepRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j // Lombok annotation to give us the 'log' object automatically!
@@ -17,10 +22,17 @@ public class WebhookEventConsumer {
 
     private final WorkflowRepository workflowRepository;
     private final ExecutionRunRepository executionRunRepository;
+    private final WorkflowStepRepository workflowStepRepository;
+    private final KafkaTemplate<String, TaskExecutionEvent> kafkaTemplate;
 
-    public WebhookEventConsumer(WorkflowRepository workflowRepository, ExecutionRunRepository executionRunRepository) {
+    public WebhookEventConsumer(WorkflowRepository workflowRepository,
+                                ExecutionRunRepository executionRunRepository,
+                                WorkflowStepRepository workflowStepRepository,
+                                KafkaTemplate<String, TaskExecutionEvent> kafkaTemplate) {
         this.workflowRepository = workflowRepository;
         this.executionRunRepository = executionRunRepository;
+        this.workflowStepRepository = workflowStepRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     // Listens to the exact topic the Ingestion Service writes to
@@ -38,20 +50,44 @@ public class WebhookEventConsumer {
                 workflow -> {
                     log.info("✅ SUCCESS! Found matching workflow: {}", workflow.getName());
 
+                    UUID trackingId = UUID.fromString(event.getTraceId());
+
                     ExecutionRun executionRun = new ExecutionRun();
-                    executionRun.setId(UUID.fromString(event.getTraceId()));
+                    executionRun.setId(trackingId);
                     executionRun.setWorkflow(workflow);
                     executionRun.setTriggerPayload(event.getPayload());
                     executionRun.setStatus(RunStatus.RUNNING);
                     executionRunRepository.save(executionRun);
 
                     log.info("Execution Run {} saved to database!", executionRun.getId());
+
+                    getWorkflowStep(workflow.getId(), 1).ifPresentOrElse(
+                            step -> {
+                                TaskExecutionEvent taskEvent = TaskExecutionEvent.builder()
+                                        .stepId(step.getId())
+                                        .webhookPayload(event.getPayload())
+                                        .actionType(step.getActionType())
+                                        .stepConfig(step.getConfiguration())
+                                        .executionRunId(executionRun.getId())
+                                        .build();
+
+                                kafkaTemplate.send("task-execution", trackingId.toString(), taskEvent);
+
+                                log.info("Task step1 {} sent to Kafka!", trackingId);
+                            },
+                            () -> log.warn("⚠️ No Step 1 found for workflow {}", workflow.getId())
+                    );
+
                 },
                 () -> {
                     log.warn("❌ FAILED: No workflow exists in the database for Trigger ID: {}", event.getWebhookId());
-                    // In the real world, we just drop the message or put it in a DLQ
                 }
         );
         log.info("=================================================\n");
+    }
+
+
+    private Optional<WorkflowStep> getWorkflowStep(UUID workflowId, int stepOrder) {
+        return workflowStepRepository.getStepByWorkflowIdAndStepOrder(workflowId, stepOrder);
     }
 }
--- orchestrator-service/src/main/java/com/zync/orchestratorservice/repository/WorkflowStepRepository.java
+++ orchestrator-service/src/main/java/com/zync/orchestratorservice/repository/WorkflowStepRepository.java
@@ -4,8 +4,10 @@ import com.zync.orchestratorservice.entity.WorkflowStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WorkflowStepRepository extends JpaRepository<WorkflowStep, UUID> {
+    Optional<WorkflowStep> getStepByWorkflowIdAndStepOrder(UUID workflowId, Integer stepOrder);
 }
